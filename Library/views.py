238787c83ef8515aeeb405577980e71ff35664e8
Bernd Wechner <bernd.wechner@gmail.com>
2019-01-25 17:35:29 +1100

    Comparing SQL to confirm the SQL generation bug
    Following the tip here:
    
    https://docs.djangoproject.com/en/2.1/faq/models/#how-can-i-see-the-raw-sql-queries-django-is-running
    
    we can see the SQL that Django executes. And compare it wity what the __str_ fucntion on queryset.query returns. And true to form they differ, and they should not!

--- Library/views.py
+++ Library/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import DetailView, ListView, UpdateView
 from django.utils import timezone
-from django.forms import ModelForm,inlineformset_factory
+from django.forms import inlineformset_factory
+from django.db import connection
 from Library.models import Author, Book, Chapter, Article
     
 class AuthorDetailView(DetailView):
@@ -50,8 +51,12 @@ class BookListView(ListView):
 
     def get_queryset(self, *args, **kwargs):
         qs = self.model.objects.filter(title='My Life')
+        
         print("Queryset returns {0} items.".format(len(qs)))
+        print("The executed SQL was:\n {}".format(connection.queries[0]['sql']))
         sql = str(qs.query)
+        print("The SQL that queryset.query returns is:\n {}".format(sql))
+
         raw_qs = self.model.objects.raw(sql)
         print("Raw queryset returns {0} items.".format(len(raw_qs)))
         return qs
